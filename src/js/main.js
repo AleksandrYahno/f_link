f162196aba318dba09cf77fd306b9d8a833eea37
AleksandrYahno <aleksandryahno@gmail.com>
2019-03-03 21:34:25 +0200

    5

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,8 @@ $(document).ready(function ($) {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 3000,
+        prevArrow: '<button id="prev" type="button" class="btn btn-juliet"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button id="next" type="button" class="btn btn-juliet"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>'
     });
 
 
@@ -33,6 +35,13 @@ $(document).ready(function ($) {
             a[i].setAttribute("class", "");
         }
         $(this).addClass("active-btn");
+    });
+
+    $(".popUp").click(function () {
+        $("#popUp_box").addClass("popUp-form-active");
+    });
+    $("#popUpClose").click(function () {
+        $("#popUp_box").removeClass("popUp-form-active");
     });
 
 

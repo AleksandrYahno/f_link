c7e6e7c6cdd1ae4a9b6326a64f34c658b1a196e6
AleksandrYahno <aleksandryahno@gmail.com>
2019-03-03 19:59:37 +0200

    4

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,11 @@
 $(document).ready(function ($) {
 
 
-    $('.single-item').slick({
+    $('.slider').slick({
         dots: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
     });
 
 
@@ -17,6 +20,14 @@ $(document).ready(function ($) {
     });
 
     $(".currency a").click(function () {
+        var a = $(this).parent().parent().find("a");
+        for(var i = 0; i < a.length; i++) {
+            a[i].setAttribute("class", "");
+        }
+        $(this).addClass("active-btn");
+    });
+
+    $(".main-menu-list a").click(function () {
         var a = $(this).parent().parent().find("a");
         for(var i = 0; i < a.length; i++) {
             a[i].setAttribute("class", "");
